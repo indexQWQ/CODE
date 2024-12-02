e646462c00784901adae311c7226f2dcd3a8523f
indexQWQ <3517905471@QQ.COM>
2024-12-02 09:41:16 +0800

    666

---
+++ test/鹏哥/鹏哥作业/x图案.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+int main() 
+{
+    int n=0;
+    scanf("%d",&n);
+    int i=0;
+    for(i=0;i<n;i++)
+    {
+        int j=0;
+        for(j=0;j<n;j++)
+        {
+            if(j==i || j==(-i+n-1))
+            {
+                printf("*");
+            }
+            else
+            {
+                printf(" ");
+            }
+        }
+        putchar('\n');
+    }
+    return 0;
+}
+
+// *   *
+//  * * 
+//   *  
+//  * * 
+// *   *
+
+
+// *    *
+//  *  * 
+//   **  
+//   **  
+//  *  * 
+// *    *
