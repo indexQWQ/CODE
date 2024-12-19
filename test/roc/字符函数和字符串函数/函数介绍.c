554d60904662d7a3b5a8e369be5efbe73a2162e7
indexQWQ <3517905471@QQ.COM>
2024-12-19 09:43:07 +0800

    666

--- test/roc/字符函数和字符串函数/函数介绍.c
+++ test/roc/字符函数和字符串函数/函数介绍.c
@@ -424,24 +424,168 @@
 // 第一个参数指定一个字符串，它包含了0个或者多个由sep字符串中一个或者多个分隔符分割的标记。
 // strtok函数找到str中的下一个标记，并将其用\0 结尾，返回一个指向这个标记的指针。#####注意会改变字符串#####
 // （注：strtok函数会改变被操作的字符串，所以在使用strtok函数切分的字符串一般都是临时拷贝的内容并且可修改。）
-// strtok函数的第一个参数不为NULL ，函数将找到str中第一个标记，strtok函数将保存它在字符串中的位置。
+// strtok函数的第一个参数不为NULL ，函数将找到str中第一个标记，strtok函数将保存(static)它在字符串中的位置。
 // strtok函数的第一个参数为NULL ，函数将在同一个字符串中被保存的位置开始，查找下一个标记。
 // 如果字符串中不存在更多的标记，则返回NULL 指针。
+// int main()
+// {
+//     const char*sep="@.";
+//     char email[]="3517905471@qq.com";//#####注意会改变字符串#####"3517905471@@qq.com",函数会直接把两个分隔符跳过
+//     char cpy[20]={0};
+//     strcpy(cpy,email);
+//     //strtok(cpy,sep);
+
+//     char *ret=strtok(cpy,sep);
+//     printf("%s\n",ret);
+//     while(ret)
+//     {
+//         ret=strtok(NULL,sep);
+//         printf("%s\n",ret);
+//     }  
+//     return 0;
+// }
+
+
+
+// strerror
+// char * strerror ( int errnum );
+// 返回错误码，所对应的错误信息。
+// #include <errno.h>//必须包含的头文件
+// int main()
+// {
+//     // errno -c语言设置的一个全局的错误码存放变量
+//     FILE *pf=fopen("C:\\Users\\Lenovo\\OneDrive\\desktop\\CODE1\\test\\roc\\字符函数和字符串函数\\test.txt","r");//绝对路径
+//     if(pf==NULL)
+//     {
+//         printf("%s\n",strerror(errno));
+//         return 1;
+//     }
+//     else
+//     {
+//         ;
+//     }
+//     return 0;
+// }
+
+
+
+
+
+
+
+
+
+
+
+// 字符分类函数：
+// 函数         如果他的参数符合下列条件就返回真
+// iscntrl     任何控制字符
+// isspace     空白字符：空格' '，换页'\f'，换行'\n'，回车'\r'，制表符'\t'或者垂直制表符'\v'
+// isdigit     十进制数字 0~9
+// isxdigit    十六进制数字，包括所有十进制数字，小写字母a~f，大写字母A~F
+// islower     小写字母a~z
+// isupper     大写字母A~Z
+// isalpha     字母a~z或A~Z
+// isalnum     字母或者数字，a~z,A~Z,0~9
+// ispunct     标点符号，任何不属于数字或者字母的图形字符（可打印）
+// isgraph     任何图形字符
+// isprint     任何可打印字符，包括图形字符和空白字符
+
+// 字符转换：
+// int tolower ( int c );
+// int toupper ( int c );
+
+
+
+
+
+
+// memcpy//内存拷贝
+// strcpy or strncpy 只能拷贝字符串
+// void * memcpy ( void * destination, const void * source, size_t num );
+// 函数memcpy从source的位置开始向后复制num个字节的数据到destination的内存位置。
+// 这个函数在遇到'\0' 的时候并不会停下来。
+// 如果source和destination有任何的重叠，复制的结果都是未定义的。
+// memcpy 负责拷贝两块独立空间中的数据
+// 但是在不同的编译器里面memcpy 也可以拷贝源内存块和目标内存块是重叠的情况
+// 模拟实现
+// void * my_memcpy ( void * destination, const void * source, size_t num )
+// {
+//     // assert(destination && source);
+//     size_t count=0;
+//     while(count<num)
+//     {
+//         *((char *)destination+count)=*((char *)source+count);
+//         count++;
+//     }
+//     return destination;
+// }
+// void test ()
+// {
+//     int arr1[]={1,2,3,4,5,6,7,8,9};
+//     int arr2[10]={0};
+//     int *p=my_memcpy(arr2,arr1,sizeof(int)*5);
+//     int i=0;
+//     for(;i<10;i++)
+//     {
+//         printf("%d ",p[i]);
+//     }
+// }
+// void test2()
+// {
+//     int arr1[]={1,2,3,4,5,6,7,8,9};
+//     my_memcpy(arr1+2,arr1,sizeof(int)*5);
+//     int i=0;
+//     for(;i<10;i++)
+//     {
+//         printf("%d ",arr1[i]);
+//     }
+// }
+// int main()
+// {
+//     //test();
+//     test2();
+//     return 0;
+// }
+
+
+
+// memmove
+// void * memmove ( void * destination, const void * source, size_t num );
+// 和memcpy的差别就是memmove函数处理的源内存块和目标内存块是可以重叠的。
+// 如果源空间和目标空间出现重叠，就得使用memmove函数处理。
+// 模拟实现
+void * my_memmove ( void * destination, const void * source, size_t num )
+{
+    // assert(destination && source);
+    size_t count=0;
+    while(count<num)
+    {
+        *((char *)destination+count)=*((char *)source+count);
+        count++;
+    }
+    return destination;
+}
+void test2()
+{
+    int arr1[]={1,2,3,4,5,6,7,8,9,10};
+    my_memmove(arr1+2,arr1,sizeof(int)*5);
+    int i=0;
+    for(;i<10;i++)
+    {
+        printf("%d ",arr1[i]);
+    }
+}
 int main()
 {
-    const char*sep="@.";
-    char email[]="3517905471@qq.com";//#####注意会改变字符串#####
-    char cpy[20]={0};
-    strcpy(cpy,email);
-    //strtok(cpy,sep);
-
-    char *ret=strtok(cpy,sep);
-    printf("%s\n",ret);
-    while(ret)
-    {
-        ret=strtok(NULL,sep);
-        printf("%s\n",ret);
-        
-    }  
+    test2();
     return 0;
 }
+
+
+
+// memcmp
+// int memcmp ( const void * ptr1, const void * ptr2, size_t num );
+// 比较从ptr1和ptr2指针开始的num个字节
+
+
