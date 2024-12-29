78aff98c916296089cb71bfccf325af778fb118c
indexQWQ <3517905471@QQ.COM>
2024-12-29 21:50:38 +0800

    666

--- test/oj great demon realm/欢乐的秋游.c
+++ test/oj great demon realm/欢乐的秋游.c
@@ -1,52 +1,56 @@
+
 #include <stdio.h>
 
-//void print(int *arr,int n)
-//{
-//	int i=0;
-//	for(i=0;i&lt;n;i++)
-//	{
-//		printf("%d ",*(arr+i));
-//	}
-//}
+#define MAX 100000
 int main()
 {
-	int n=0,count=0;
-	scanf("%d",&n);
-	int i=0;
-	int arr[1000]={0};
-	for(i=0;i<n;i++)
-	{
-		scanf("%d",&arr[i]);
-	}
-	int arr1[n-3];
-	int j=0;
-	for(i=1;i<n-2;i++)
-	{
-		arr1[j++]=arr[i]-arr[i+1];
-	}
-	for(i=0;i<n-3;i++)
-	{
-		if(arr1[i]==0)
-		{
-			int j=i;
-			for(;j<n-4;j++)
-			{
-				int temp=arr1[j];
-				arr1[j]=arr1[j+1];
-				arr1[j+1]=temp;
-			}
-			i--;
-			n--;
-		}
-	}
-	//print(arr1,n-3);
-	for(i=0;i<n-4;i++)
-	{
-		if(arr1[i]>0 && arr1[i+1]<0)
-		{
-			count++;
-		}
-	}
-	printf("%d\n",count);
-	return 0;
+    int arr[MAX]={0};
+    int arrr[MAX]={0};
+    int n=0;
+    scanf("%d",&n);
+    int i=0;
+    int flag=1;
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    int j=0;
+    int count=0;
+    for(i=1;i<n-1;i++)
+    {
+        flag=0;
+        arrr[j]=arr[i]-arr[i+1];
+        //printf("%d ",arr[i]);
+        j++;
+    }
+    for(j=0;j<n-3;j++)
+    {
+        if(arrr[j]==0)
+        {
+            int i=0;
+            for(i=j;i<n-4;i++)
+            {
+                arrr[j]=arrr[j+1];
+            }
+            n--;
+            j--;
+        }
+    }
+    for(j=0;j<n-3;j++)
+    {
+        if(arrr[j]>0 && arrr[j+1]<0)
+        {
+            count++;
+        }
+        //printf("%d ",arrr[j]);
+    }
+    if(flag)
+    {
+        printf("0");
+    }
+    else
+    {
+        printf("%d",count);
+    }
+    return 0;
 }
