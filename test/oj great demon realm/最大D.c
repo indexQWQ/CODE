64abad3e5aef6220a54370c92389af2bd4c5bb6c
indexQWQ <3517905471@QQ.COM>
2025-01-10 22:53:54 +0800

    666

--- test/oj great demon realm/最大D.c
+++ test/oj great demon realm/最大D.c
@@ -1,115 +1,77 @@
-// long long caculate(long long sum,int len)
-// {
-//     if(len==1)
-//     {
-//         return sum;
-//     }
-//     if(sum==0)
-//     {
-//         return 0;
-//     }
-//     long long temp=sum;
-//     long long reverse=0;
-//     while(temp>0)
-//     {
-//         reverse=reverse*10+temp%10;
-//         temp/=10;
-//     }
-//     if(reverse>sum)
-//     {
-//         return (sum%10)*pow(10,len-1)+caculate(sum/10,len-1);
-//     }
-//     else if(reverse<sum)
-//     {
-//         return (reverse%10)*pow(10,len-1)+caculate(reverse/10,len-1);
-//     }
-//     else
-//     {
-//         return (sum%10)*pow(10,len-1)+caculate(sum/10,len-1);
-//     }
-//     return 0;
-// }
-// int main()
-// {
-//     char arr[100]={0};
-//     scanf("%s",arr);
-//     int len=strlen(arr);
-//     //printf("%d",len);
-//     long long sum=0;
-//     int count=0;
-//     int i=0;
-//     for(i=0;i<len;i++)
-//     {
-//         if(!isalpha(arr[i]))
-//         {
-//             sum=sum*10+(arr[i]-'0');
-//             count++;
-//         }
-//     }
-//     long long a=caculate(sum,count);
-//     printf("%lld",a);
-//     return 0;
-// }
-
-
-
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include <math.h>
-
-void caculate(char *arr1,char *arr2,int left,int right)
+int find(int *arr,int i,int j)
 {
-    int count=0;
-    while(left<=right)
+    while(i<=j)
     {
-        if(*(arr1+left)<*(arr1+right))
+        if(arr[i]>arr[j])
         {
-            *(arr2+count)=*(arr1+right);
-            count++;
-            right--;
-            continue;
+            return 1;
         }
-        else if(*(arr1+left)>*(arr1+right))
+        else if(arr[i]<arr[j])
         {
-            *(arr2+count)=*(arr1+left);
-            count++;
-            left++;
-            continue;
+            return 2;
         }
-        else if(*(arr1+left)==*(arr1+right))
+        i++,j--;
+    }
+    return 0;
+}
+// 1j2j3j4j5j6j4j4j2j1   1244654321
+void caculate(int *arr,int len,int *big,int *len2)
+{
+    int i=0,j=len-1;
+    for(i=0;i<len && j>=0;)
+    {
+        if(arr[i]>arr[j])
         {
-            *(arr2+count)=*(arr1+right);
-            break;
+            big[(*len2)++]=arr[i++];
+        }
+        else if(arr[i]<arr[j])
+        {
+            big[(*len2)++]=arr[j--];
+        }
+        else
+        {
+            int flag=find(arr,i+1,j-1);
+            if(flag==1)
+            {
+                big[(*len2)++]=arr[i++];
+            }
+            else if(flag==2)
+            {
+                big[(*len2)++]=arr[j--];
+            }
+            else
+            {
+                big[(*len2)++]=arr[i++];
+            }
         }
     }
 }
 int main()
 {
     char arr[100]={0};
-    char arr1[100]={0};
-    char arr2[100]={0};
     scanf("%s",arr);
     int len=strlen(arr);
     //printf("%d",len);
-    int count=0;
-    int i=0;
+    int sum[100]={0};
+    int count=0,count1=0,i=0,big[100]={0};
     for(i=0;i<len;i++)
     {
         if(!isalpha(arr[i]))
         {
-            arr1[count]=arr[i];
-            count++;
+            sum[count++]=(arr[i]-'0');
         }
     }
-    if(count==0)
+    // for(i=0;i<count;i++)
+    // {
+    //     printf("%d",sum[i]);
+    // }putchar('\n');
+    caculate(sum,count,big,&count1);
+    for(i=0;i<count;i++)
     {
-        printf("0");
-        return 0;
+        printf("%d",big[i]);
     }
-    int left=0;
-    int right=count-1;
-    caculate(arr1,arr2,left,right);
-    printf("%s",arr2);
     return 0;
 }
