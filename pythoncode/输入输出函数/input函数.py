7bb3ef789d0ee669df78698c74e70285b621688a
indexQWQ <3517905471@QQ.COM>
2025-03-22 16:25:46 +0800

    888

---
+++ pythoncode/输入输出函数/input函数.py
@@ -0,0 +1,12 @@
+#语法x=input('提示词')
+#注意事项无论输入的数据是什么
+#x的数据类型都是字符串类型
+
+# name=input("input you name:")
+# print("my name is:"+name)
+
+#输入整数类型的数据
+num=input("input digit:")
+print('val is:'+num)
+num=int(num)#使用内置函数int将num转换成整数
+#print('val is:'+num)#err
