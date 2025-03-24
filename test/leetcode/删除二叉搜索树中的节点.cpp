57809dc92b0d33dcf5844c81270990ac0437c5ad
indexQWQ <3517905471@QQ.COM>
2025-03-24 23:04:50 +0800

    888

--- test/leetcode/删除二叉搜索树中的节点.cpp
+++ test/leetcode/删除二叉搜索树中的节点.cpp
@@ -27,6 +27,34 @@ struct TreeNode {
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        
+        if(!root)return nullptr;
+        if(root->val==key){
+            if(!root->left && !root->right){
+                delete root;
+                return nullptr;
+            }
+            else if(root->left && !root->right){
+                TreeNode* temp=root->left;
+                delete root;
+                return temp;
+            }
+            else if(!root->left && root->right){
+                TreeNode* temp=root->right;
+                delete root;
+                return temp;
+            }
+            else{
+                TreeNode* temp=root->right;
+                while(temp->left)temp=temp->left;
+                temp->left=root->left;
+                TreeNode* dtemp=root;
+                root=root->right;
+                delete dtemp;
+                return root;
+            }
+        }
+        if(root->val>key) root->left=deleteNode(root->left,key);
+        if(root->val<key) root->right=deleteNode(root->right,key);
+        return root;
     }
 };
