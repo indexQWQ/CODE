7523ab5e9b5c21ccd3490de70afdfc543d661d60
indexQWQ <3517905471@QQ.COM>
2025-03-28 23:09:17 +0800

    888

---
+++ pythoncode/数据类型和运算符/类型.py
@@ -0,0 +1,15 @@
+height=177.7
+print(type(height))
+
+x=10.0
+y=1.22e2
+print(type(x),type(y))
+print(y)
+
+print(0.1+0.2)#不确定的尾数问题0.30000000000000004
+print(round(0.1+0.2,1))
+
+#复数类型的定义
+x=123+222j
+print(x.real)
+print(x.imag)
---
+++ pythoncode/空语句pass.py
@@ -0,0 +1,9 @@
+if True:
+    pass
+
+while False:
+    pass
+
+for i in range(10):
+    print(i,end=' ')
+    pass
