24e4213f13bf6728faab2d5a7facc2a2823d866d
indexQWQ <3517905471@qq.com>
2025-04-20 23:05:59 +0800

    888

---
+++ test/leetcode/买卖股票的最佳时机 II.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() < 2) return 0;
+
+        int sum = 0;
+        for (int i = 0; i < prices.size() - 1; i++) {
+            if (prices[i + 1] > prices[i]) {
+                sum += prices[i + 1] - prices[i];
+            }
+        }
+        return sum;
+    }
+};
